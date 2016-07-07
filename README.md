da0a9259b43a8b2aaaf8d3749c2b39d976367c62
kaiwang0112006 <kaiwang0112006@yahoo.com>
2016-07-07 10:09:23 +0800

    init

---
+++ README.md
@@ -0,0 +1,34 @@
+# netcontig
+
+Build a net of contig and find the shortest path. Please see the examples input and output format
+
+
+script path on server
+---------
+/public/scripts/kw/netcontig/contigpath.py
+
+Usage
+---------
+
+$ python src/contigpath.py -h
+
+usage: contigpath.py [-h] --input INPUT --start START --end END [--out OUTPUT]
+
+python *.py [option]
+
+optional arguments:
+
+  -h, --help     show this help message and exit
+
+  --out OUTPUT   output file
+
+required arguments:
+
+  --input INPUT  input file to insert into DB,required
+
+  --start START  start nodes file
+
+  --end END      end nodes file
+
+
+python contigpath.py  --input=/public/share/hldu/R498_Final_Final_Ref/R498_Analysis/R498_Pacbio_Self/Graph/Start_End.txt --start=start.txt --end=end.txt
---
+++ src/contigpath.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+import networkx as nx
+import time
+import argparse
+import csv
+
+##########################################
+## Options and defaults
+##########################################
+def getOptions():
+    parser = argparse.ArgumentParser(description='python *.py [option]"')
+    requiredgroup = parser.add_argument_group('required arguments')
+    requiredgroup.add_argument('--input', dest='input', help='input file to insert into DB,required', default='', required=True)
+    requiredgroup.add_argument('--start', dest='start', help='start nodes file', default='', required=True)
+    requiredgroup.add_argument('--end', dest='end', help='end nodes file', default='', required=True)
+    parser.add_argument('--out',dest='output',help='output file', default='output.out')
+    args = parser.parse_args()
+
+    return args
+
+class CONTIGGRAPH(object):
+    def __init__(self):
+        self.G = nx.Graph()
+        
+    def buildGraph(self, input):
+        '''
+        Fetch all the source and tartget id from input file to build a net
+        '''
+        with open(input) as fin:
+            for eachline in fin:
+                line = eachline.split()
+                if line[0] == '0' or line[1] == '0':
+                    print "abnormal line:",eachline
+                self.G.add_node(line[0])
+                self.G.add_node(line[1])           
+                self.G.add_edge(line[0],line[1])
+                
+    def shortCal(self,start, end):
+        self.allspath = nx.all_shortest_paths(self.G,source=start,target=end)
+        
+    def allshortpath(self,startlist,endlist):
+        '''
+        For each start and end pair in the list, calculate all shortest paths and formate dict to store them.
+        i,j are list index and s,e are ids.
+        '''
+        
+        self.allspath = {}
+        for i, s in enumerate(startlist):
+            for j,e in enumerate(endlist):
+                 self.allspath[(s,e)] = nx.all_shortest_paths(self.G,source=s,target=e)
+        
+    def formateOutput(self, output):
+        '''
+        With the dict that have id pairs and their shortest paths, an output will be generated.
+        eachline as a csv format:
+        startid,endid,path
+        
+        path is formate as id1|id2|....|idn
+        '''  
+        fout = open(output,'w')
+        for pair in self.allspath:
+            s = pair[0]
+            t = pair[1]
+            try:
+                for p in self.allspath[pair]:
+                    pstr = '|'.join(p)
+                    fout.write("%s,%s,%s\n" % (s,t,pstr))
+            except:
+                fout.write("%s,%s,nopath\n" % (s,t))
+                
+def readids(file):
+    '''
+    a file contain start or end id will be parsed.
+    file like:
+    ...
+    R498_5022013_1/1833_21834
+    R498_4314669_1/8_17103
+    ...
+    
+    will be stored in a list:
+    [...,'R498_5022013_1/1833_21834','R498_4314669_1/8_17103',....]
+    '''
+    idlist = []
+    with open(file) as fin:
+        for eachline in fin:
+            id = eachline.strip()
+            idlist.append(id)
+    return idlist
+            
+
+##########################################
+## Master function
+##########################################           
+def main():
+    options = getOptions()
+    print "parsing start/end nodes file..."
+    startlist = readids(options.start)
+    endlist = readids(options.end)
+
+    contiggraph = CONTIGGRAPH()
+    print "building graph..."
+    contiggraph.buildGraph(options.input)
+    print "complete graph"
+    print "cal short path..."
+    contiggraph.allshortpath(startlist, endlist)
+    print "Formating output...."
+    contiggraph.formateOutput(options.output)
+
+
+
+
+    
+if __name__ == "__main__":
+    main()
